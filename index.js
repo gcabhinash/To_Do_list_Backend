8b59ec2db614c8889079b9d5a2c72225b12d3081
gcabhinash <gcabhinash@gmail.com>
2025-07-02 18:40:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
